87a9047f69c3dc3ec81b03e7c519cd46724897eb
andres <andres06energy@hotmail.com>
2022-04-14 17:23:53 -0300

    First Commit

---
+++ OneDrive/Escritorio/LenguajesProgramacion/JAVA_SPRING/Proyectos2022/AppSupermercadoOnline/ApiRest_SupermercadoOnline/src/main/java/com/supermercado/online/ApiRestSupermercadoOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.supermercado.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSupermercadoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSupermercadoOnlineApplication.class, args);
+	}
+
+}
---
+++ OneDrive/Escritorio/LenguajesProgramacion/JAVA_SPRING/Proyectos2022/AppSupermercadoOnline/ApiRest_SupermercadoOnline/src/test/java/com/supermercado/online/ApiRestSupermercadoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermercado.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSupermercadoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
