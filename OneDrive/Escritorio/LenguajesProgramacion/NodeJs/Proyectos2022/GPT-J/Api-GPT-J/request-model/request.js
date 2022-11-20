7362f342424c7d67aef97142e25a029342e27cb8
Andrés Weitzel <andres96energy@hotmail.com>
2022-11-19 22:43:50 -0300

    Updated Repository

---
+++ OneDrive/Escritorio/LenguajesProgramacion/NodeJs/Proyectos2022/GPT-J/Api-GPT-J/request-model/request.js
@@ -0,0 +1,20 @@
+const banana = require('@banana-dev/banana-dev');
+const config = require('../config.js');
+
+var apiKey = config.API_KEY;
+var modelKey = "gptj";
+
+const modelParameters = {
+        "text": "Hey GPTJ, how are you doing?",
+        "length":250,
+        "temperature":0.9,
+        "batchSize": 1
+}
+
+let run = async (modelParameters) => {
+    var out = await banana.run(apiKey, modelKey, modelParameters)
+    console.log(out)
+    return out
+}
+
+run(modelParameters)
